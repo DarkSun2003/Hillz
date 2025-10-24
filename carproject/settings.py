17a9fc6624f9e861ba009adbccf5d79640052fa8
DarkSun2003 <damilareogunnowo10@gmail.com>
2025-10-24 18:26:42 +0100

    work please

--- carproject/settings.py
+++ carproject/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
         'NAME': 'railway',
         'USER': 'postgres',
         'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': 'shuttle.proxy.rlwy.net',
-        'PORT': '12338',
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
         'DATABASE_PUBLIC_URL': os.environ['DB_URL']
     }
 }
