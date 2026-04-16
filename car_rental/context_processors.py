7fe2e3c668d40d57f3fd65fc14c8ee2d626e239e
DarkSun2003 <damilareogunnowo10@gmail.com>
2026-04-17 00:55:35 +0100

    Google OAuth credentials and email settings have been updated in the .env file. Additionally, the context processor has been modified to include site information, and a new view for fleet management has been added to the views.py file. The urls.py file has also been updated to include routes for fleet management and quick status updates for cars. Finally, the settings.py file has been updated to enforce email verification and to allow automatic linking of Google accounts based on email addresses.

--- car_rental/context_processors.py
+++ car_rental/context_processors.py
@@ -1,7 +1,18 @@
 from .models import SiteInfo
+from django.conf import settings
 
 def get_site_info_context(request):
     """
     Add site_info to all templates
     """
     return {'site_info': SiteInfo.objects.first()}
+
+
+def marketing_keys(request):
+    """
+    Makes marketing API keys globally available to all templates.
+    """
+    return {
+        'GOOGLE_ANALYTICS_ID': getattr(settings, 'GOOGLE_ANALYTICS_ID', ''),
+        'META_PIXEL_ID': getattr(settings, 'META_PIXEL_ID', ''),
+    }
